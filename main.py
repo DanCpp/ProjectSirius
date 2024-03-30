49a51d1af96fdff3924ac289fa9a7dc687546d98
Danila Ushakov <danila.ushakov.08@mail.ru>
2024-03-31 00:52:02 +0800

    Done Model

---
+++ main.py
@@ -0,0 +1,39 @@
+from transformers import pipeline
+from openpyxl import load_workbook
+
+classifier = pipeline("sentiment-analysis",   
+                      "blanchefort/rubert-base-cased-sentiment")
+
+Book = load_workbook("Train.xlsx")
+
+rev_sheet = Book["Rev"]
+
+revs = []
+
+count = 0
+
+for item in rev_sheet:
+    count += 1
+
+
+for i in range(2, count + 1):
+    text = ''.join(rev_sheet["B" + str(i)].value.replace(u'\xa0', u' ').replace(u'\n', u' '))
+    if len(text) > 512:
+        text = text[:(512-len(text))]
+    revs.append(text)
+
+
+tonal = []
+
+for review in revs:
+    verdict = classifier(review)[0]
+    if verdict['label'] == 'NEUTRAL':
+        verdict['label'] = 'NEGATIVE'
+    to = verdict['label']
+    tonal.append(to)
+
+for i in range(len(tonal)):
+    rev_sheet["C" + str(i + 2)] = tonal[i]
+
+Book.save("Train.xlsx")
+
